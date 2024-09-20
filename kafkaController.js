e529a6e8da4120329225aa74ef642a2fc7a656ea
Derrick Mbugua <derrickmbugua50@gmail.com>
2024-09-20 14:42:53 +0300

    feat: publish and consume message

--- kafkaController.js
+++ kafkaController.js
@@ -28,10 +28,51 @@ class KafkaController {
         ],
       });
       await admin.disconnect();
-      
     } catch (error) {
       console.log("Error createTopic:", error);
       throw error;
+    }
+  }
+
+  //   publish message to the topic
+  async publishMessageToTopic(topicName, messages) {
+    const producer = this.kafka.producer();
+    try {
+      await producer.connect();
+      await producer.send({
+        topic: topicName,
+        messages,
+      });
+    } catch (error) {
+      console.log("Error in publish message", error);
+      throw error;
+    } finally {
+      await producer.disconnect();
+    }
+  }
+
+  //   consume message
+  async consumeMessageFromTopic(topicName, callback) {
+    const consumer = this.kafka.consumer({groupId: 'test-group'});
+    try {
+        await consumer.connect();
+        await consumer.subscribe({
+            topic: topicName,
+            fromBeginning: true
+        });
+        await consumer.run({
+            eachMessage: async ({
+                topic,
+                partition,
+                message
+            }) => {
+                const value = `Received message: ${message.value.toString()} from partition ${partition} & topic ${topic}`;
+                callback(value);
+            }
+        })
+    } catch (error) {
+        console.log("Error in consume message:", error);
+        throw error;
     }
   }
 }
--- routes.js
+++ routes.js
@@ -3,21 +3,57 @@ import KafkaController from "./kafkaController.js";
 
 const router = Router();
 
+router.post("/create_topic", async (req, res) => {
+  try {
+    const { topicName, noOfPartition } = req.body;
+    const kafkaController = new KafkaController();
+    await kafkaController.createTopic(topicName, noOfPartition);
+    res.send({
+      status: "Ok",
+      message: "Topic Successfully Created",
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Failed to create Topic",
+    });
+  }
+});
 
-router.post('/create_topic', async(req, res) => {
-    try {
-        const { topicName, noOfPartition } = req.body;
-        const kafkaController = new KafkaController();
-        await kafkaController.createTopic(topicName,noOfPartition);
-        res.send({
-            status: "Ok",
-            message: "Topic Successfully Created",
-          });
-    } catch (error) {
-        res.status(500).send({
-            message: "Failed to create Topic"
-          });
-    }
+router.post("/publish", async (req, res) => {
+  try {
+    const { topicName, message } = req.body;
+    const messages = [{
+        key: message?.key,
+        value: message?.value
+    }]
+    const kafkaController = new KafkaController();
+    await kafkaController.publishMessageToTopic(topicName, messages);
+    res.send({
+      status: "Ok",
+      message: "Message Successfully published",
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Failed to publish message",
+    });
+  }
+});
+
+router.post("/consume", async (req, res) => {
+  try {
+    const { topicName } = req.body;
+    const kafkaController = new KafkaController();
+    await kafkaController.consumeMessageFromTopic(topicName, (message) => {
+      res.send({
+        status: "Ok",
+        message,
+      });
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: "Failed to consume message",
+    });
+  }
 });
 
 export default router;
